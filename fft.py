c7944d5033ca31062972f7247bd14c1b6bad82b2
KūHaku <65805722+sireeshdevaraj@users.noreply.github.com>
2022-06-12 09:50:03 +0530

    Update fft.py

--- fft.py
+++ fft.py
@@ -5,10 +5,10 @@ from matplotlib import pyplot as plt
 img = cv.imread('assets/sample.jpg',0)
 f = np.fft.fft2(img)
 fshift = np.fft.fftshift(f)
-magnitude_spectrum = 20*np.log(np.abs(fshift))
+output = 20*np.log(np.abs(fshift))
 plt.subplot(121),plt.imshow(img, cmap = 'gray')
 plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.subplot(122),plt.imshow(output, cmap = 'gray')
 plt.title('OUTPUT'), plt.xticks([]), plt.yticks([])
 plt.show()
 
